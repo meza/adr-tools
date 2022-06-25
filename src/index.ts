570614ca7fc9bb944fd25e3b29498cf46d107a98
meza <meza@meza.hu>
2022-06-25 20:14:12 +0100

    chore: added TOC prefix support #13

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ program.name('adr').version(version).description('Manage Architecture Decision L
 program.command('new')
   .argument('<title...>', 'The title of the decision')
   .option('-q, --quiet', 'Do not ask for clarification. If multiple files match the search pattern, an error will be thrown.')
-  .option('-s, --supersede <SUPERSEDED>', 'A reference (number or partial filename) of a previous decision that the new decision supercedes.\n'
+  .option('-s, --supersede <SUPERSEDE>', 'A reference (number or partial filename) of a previous decision that the new decision supercedes.\n'
     + 'A Markdown link to the superceded ADR is inserted into the Status section.\n'
     + 'The status of the superceded ADR is changed to record that it has been superceded by the new ADR.', collectSupersedes, [])
   .option('-l, --link "<TARGET:LINK:REVERSE-LINK>"', 'Links the new ADR to a previous ADR.\n'
@@ -46,7 +46,8 @@ program.command('new')
 
 program.command('generate')
   .argument('toc')
-  .action(generateToc);
+  .option('-p, --prefix <PREFIX>', 'The prefix to use for each file link in the generated TOC.')
+  .action((_command, options) => generateToc(options));
 
 program.command('link')
   .argument('<SOURCE>', 'Full or Partial reference number to an ADR')
